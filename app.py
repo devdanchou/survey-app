51259080edf3650f8b82138d949fc01a1ef0e1ad
Tichin Wu <twu38@mail.ccsf.edu>
2023-07-20 15:50:58 -0700

    WIP question.html

--- app.py
+++ app.py
@@ -11,12 +11,33 @@ debug = DebugToolbarExtension(app)
 
 responses = []
 
+
 @app.get("/")
 def to_home():
     """Directs user to start the survey"""
     title = survey.title
     instructions = survey.instructions
 
-    return render_template("survey_start.html", title = title,
-                           instructions = instructions)
+    return render_template("survey_start.html", title=title,
+                           instructions=instructions)
 
+
+@app.post('/begin')
+def to_question_0():
+    """Redirect to the first question
+    and make sure there is no previous responses"""
+
+    responses.clear()
+    return redirect('/questions/0')
+
+
+@app.get('/questions/<int:qnum>')
+def to_question(qnum):
+    """Go to question with a certain number """
+
+    question = survey.questions[qnum]
+    return render_template('question.html', question=question)
+
+# post request to /answer
+#     hand over the answer from the user to the responses list
+#     redirect to the next question
